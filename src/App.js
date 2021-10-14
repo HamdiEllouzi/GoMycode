6a06a9c941ad3912311c0eb1c398abcd3114e5e4
HamdiEllouzi <hlouzi089@gmail.com>
2021-10-14 14:44:19 +0100

    checkpoint2

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import './App.css';
+import * as React from 'react';
+import SingUp from './singUp'
+import {Input,InputLabel,InputAdornment,FormControl,IconButton} from '@mui/material/';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+function App() {
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SingUp />
+      <FormControl variant="standard" sx={{ m: 1, mt: 3, width: '15ch' }}>
+        <InputLabel htmlFor="input-with-icon-adornment">
+          Username
+        </InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <FormControl  sx={{ m: 1, mt: 3, width: '15ch' }} variant="standard">
+          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            startAdornment={
+              <InputAdornment position="start">
+                <VpnKeyIcon />
+              </InputAdornment>
+            }
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </header>
+    </div>
+  );
+}
+
+export default App;
